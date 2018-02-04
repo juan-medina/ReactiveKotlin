897d404a85d93fecc4a624cfa185112fdff401a7
Juan Medina <jamedina@gmail.com>
2018-02-04 15:00:28 +0000

    completing the presentation

--- src/main/kotlin/com/juan/medina/ReactiveKotlin/CustomerHandler.kt
+++ src/main/kotlin/com/juan/medina/ReactiveKotlin/CustomerHandler.kt
@@ -1,16 +1,14 @@
 package com.juan.medina.ReactiveKotlin
 
 import org.springframework.stereotype.Component
-import org.springframework.web.reactive.function.BodyInserters.fromObject
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.body
 
 @Component
 class CustomerHandler(val customerRepository: CustomerRepository) {
-  fun getCustomer(serverRequest: ServerRequest) = customerRepository
-      .findById(serverRequest.pathVariable("id").toInt())
-      .flatMap { ServerResponse.ok().body(fromObject(it)) }
+  fun getCustomer(serverRequest: ServerRequest) = ServerResponse.ok()
+      .body<Customer>(customerRepository.findById(serverRequest.pathVariable("id").toInt()))
 
   fun getAllCustomers(serverRequest: ServerRequest) = ServerResponse.ok()
       .body<Customer>(customerRepository.getAll())
